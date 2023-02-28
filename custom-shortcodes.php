fd7b177e87546076fa4dab3ee24bbf7db7325988
Pau <pau.pulidocompanys@gmail.com>
2023-02-28 13:38:39 +0100

    feat: template for blog post

--- custom-shortcodes.php
+++ custom-shortcodes.php
@@ -20,24 +20,25 @@
     $html = '<h2 class="frontpage_news_section_title"> Noticias </h2>';
     $html .= '<div class="frontpage_news">';
     foreach ($posts as $key -> $post) {
+        $html .= '<div class="' . $key->$post->ID . '">';
         
-        $html .= '<div class="' . $key->$post->ID . '">' . '<h5>' . $key->$post->post_title . '</h5>';
         if(has_post_thumbnail( $key->$post->ID) ){ 
             $html .= get_the_post_thumbnail( $key->$post->ID);
 
         }
+        $html .= '<h5>' . $key->$post->post_title . '</h5>';
         $category = get_the_category($key->$post->ID);
         $category=$category[0];
         $date = get_the_date('j \d\e F \d\e Y', $key->$post->ID);
         $html .= '<p class="news_meta">' . $date . ' | ' . $category->name . '</p>';
-        $html .= '<p class="news_excerpt">' . $key->$post->post_excerpt . '</p>';
-        $html .= '<a href="' . get_permalink( $key->$post->ID) . '" target="_blank"> <p>Continua leyendo</p>  </a>';
+        $html .= '<p class="news_excerpt has-small-font-size">' . $key->$post->post_excerpt . '</p>';
+        $html .= '<a class="news_keep_reading has-small-font-size" href="' . get_permalink( $key->$post->ID) . '" target="_blank"> <p>Continua leyendo</p>  </a>';
         $html .= '</div>';
         // $html .= '<select class="ajax_mn_filter" id="' . $taxonomy. '" multiple="multiple"">';
         
     }
     $html .= '</div>';
-    $html .= '<a href="#" target="_blank"> <p>Ver más entradas</p>  </a>';
+    $html .= '<a class="news_archive_button" href="#" target="_blank"> <p>Ver más entradas</p>  </a>';
     return $html;
 }
 
