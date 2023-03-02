32d8d253f1de814fb2e8b397ff9b8a8b7ecdc2f5
Pau <pau.pulidocompanys@gmail.com>
2023-03-02 11:45:44 +0100

    feat: related posts query

--- custom-shortcodes.php
+++ custom-shortcodes.php
@@ -1,41 +1,42 @@
 <?php
- 
- function miradanativa_getposts_function() {
-    function get_noticies_posts( $args = null ) {
-        $posts = get_posts( array(
+
+function miradanativa_getposts_function()
+{
+    function get_noticies_posts($args = null)
+    {
+        $posts = get_posts(array(
             'post_type'    => 'post',
             'numberposts'  => 3,
             'post_status'  => 'publish'
-        ) );
+        ));
 
         // throw new Exception (print_r($posts));
         return array($posts);
-        
     }
 
-    
-    
+
+
     $posts = get_noticies_posts();
-    $posts=$posts[0];
+    $posts = $posts[0];
+    echo print_r($posts);
     $html = '<h2 class="frontpage_news_section_title"> Noticias </h2>';
     $html .= '<div class="frontpage_news">';
-    foreach ($posts as $key -> $post) {
+    foreach ($posts as $key->$post) {
         $html .= '<div class="' . $key->$post->ID . '">';
-        
-        if(has_post_thumbnail( $key->$post->ID) ){ 
-            $html .= get_the_post_thumbnail( $key->$post->ID);
 
+        if (has_post_thumbnail($key->$post->ID)) {
+            $html .= get_the_post_thumbnail($key->$post->ID);
         }
         $html .= '<h5>' . $key->$post->post_title . '</h5>';
         $category = get_the_category($key->$post->ID);
-        $category=$category[0];
+        $category = $category[0];
         $date = get_the_date('j \d\e F \d\e Y', $key->$post->ID);
         $html .= '<p class="news_meta">' . $date . ' | ' . $category->name . '</p>';
         $html .= '<p class="news_excerpt has-small-font-size">' . $key->$post->post_excerpt . '</p>';
-        $html .= '<a class="news_keep_reading has-small-font-size" href="' . get_permalink( $key->$post->ID) . '" target="_blank"> <p>Continua leyendo</p>  </a>';
+        $html .= '<a class="news_keep_reading has-small-font-size" href="' . get_permalink($key->$post->ID) . '" target="_blank"> <p>Continua leyendo</p>  </a>';
         $html .= '</div>';
         // $html .= '<select class="ajax_mn_filter" id="' . $taxonomy. '" multiple="multiple"">';
-        
+
     }
     $html .= '</div>';
     $html .= '<a class="news_archive_button" href="#" target="_blank"> <p>Ver más entradas</p>  </a>';
--- template-parts/content-post.php
+++ template-parts/content-post.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The default template for displaying content
  *
@@ -13,47 +14,52 @@
 
 ?>
 
-<article class = "single-post" <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+<article class="single-post" <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
 	<?php
 
-	get_template_part( 'template-parts/entry-header' );?>
+	get_template_part('template-parts/entry-header'); ?>
 
 	<div class="post-heading">
 		<div class="post-heading__category">
-			<p><?php $cat=get_the_category();
-			echo $cat[0]->name; ?></p>
+			<p><?php $cat = get_the_category();
+				echo $cat[0]->name; ?></p>
 		</div>
 		<div class="post-heading__post-title">
 			<h1><?php the_title(); ?></h1>
+			<?php $mypod = pods();
+			echo '<p>' . $mypod->display('subttulo') . '</p>';
+			?>
 		</div>
 		<div class="post-heading__post-details">
-			<p class="bold"><?php the_date();?> </p> <p> per </p> <p class="bold"><?php the_author();?></p>
+			<p class="bold"><?php the_date(); ?> </p>
+			<p> per </p>
+			<p class="bold"><?php the_author(); ?></p>
 		</div>
 		<div class="post-heading__feat-img">
-			<p><?php the_post_thumbnail( 'full' ); ?></p>
+			<p><?php the_post_thumbnail('full'); ?></p>
 		</div>
-	<?php
-	
-	
-	if ( ! is_search() ) {
-		// get_template_part( 'template-parts/featured-image' );
-	}
+		<?php
 
-	?>
-		
+
+		if (!is_search()) {
+			// get_template_part( 'template-parts/featured-image' );
+		}
+
+		?>
+
 	</div>
 
-	<div class="post-inner <?php echo is_page_template( 'templates/template-full-width.php' ) ? '' : 'thin'; ?> ">
+	<div class="post-inner <?php echo is_page_template('templates/template-full-width.php') ? '' : 'thin'; ?> ">
 
 		<div class="entry-content">
 
 			<?php
-			if ( is_search() || ! is_singular() && 'summary' === get_theme_mod( 'blog_content', 'full' ) ) {
+			if (is_search() || !is_singular() && 'summary' === get_theme_mod('blog_content', 'full')) {
 				the_excerpt();
 			} else {
-				
-				the_content( __( '', 'miradanativa' ) );
+
+				the_content(__('', 'miradanativa'));
 			}
 			?>
 
@@ -61,54 +67,40 @@
 
 	</div><!-- .post-inner -->
 
-	<div class="section-inner">
+	<div class="related-posts__section">
 		<?php
-		wp_link_pages(
-			array(
-				'before'      => '<nav class="post-nav-links bg-light-background" aria-label="' . esc_attr__( 'Page', 'twentytwenty' ) . '"><span class="label">' . __( 'Pages:', 'twentytwenty' ) . '</span>',
-				'after'       => '</nav>',
-				'link_before' => '<span class="page-number">',
-				'link_after'  => '</span>',
-			)
-		);
-
-		edit_post_link();
-
-		// Single bottom post meta.
-		twentytwenty_the_post_meta( get_the_ID(), 'single-bottom' );
-
-		if ( post_type_supports( get_post_type( get_the_ID() ), 'author' ) && is_single() ) {
-
-			get_template_part( 'template-parts/entry-author-bio' );
-
-		}
+		$related_query = new WP_Query(array(
+			'post_type'    => 'post',
+			// 'category__in' => wp_get_post_categories(get_the_ID()),
+			'post__not_in' => array(get_the_ID()),
+			'posts_per_page'  => 3,
+			'post_status'  => 'publish',
+			'orderby' => 'date',
+		));
 		?>
+		<h2 class="section-title"> Posts relacionados </h2>
+		<div class="related-posts">
+			<?php if ($related_query->have_posts()) { ?>
+				<div class="related-posts__grid">
+					<?php while ($related_query->have_posts()) { ?>
+						<?php $related_query->the_post(); ?>
+						<div class="grid-item">
+							<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('post-thumb-small'); ?></a>
+							<h5><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+							<p><?php the_excerpt(); ?></p>
+						</div>
+					<?php } ?>
+				</div>
+				<?php wp_reset_postdata(); ?>
+			<?php } ?>
+		</div>
 
-	</div><!-- .section-inner -->
 
-	<?php
+	</div>
 
-	if ( is_single() ) {
 
-		get_template_part( 'template-parts/navigation' );
 
-	}
 
-	/**
-	 *  Output comments wrapper if it's a post, or if comments are open,
-	 * or if there's a comment number – and check for password.
-	 * */
-	if ( ( is_single() || is_page() ) && ( comments_open() || get_comments_number() ) && ! post_password_required() ) {
-		?>
 
-		<div class="comments-wrapper section-inner">
-
-			<?php comments_template(); ?>
-
-		</div><!-- .comments-wrapper -->
-
-		<?php
-	}
-	?>
 
 </article><!-- .post -->
