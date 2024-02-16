335e56c5a5e8144d9c6905567d50352dd4efff99
Lucas García <lucas@codeccoop.org>
2024-02-16 11:15:41 +0100

    feat: ajax button layouts

--- single-user.php
+++ single-user.php
@@ -16,6 +16,27 @@ get_header();
       <?= do_shortcode('[mn_filmmarks_list]') ?>
     </div>
   </div>
+    <style>
+    article.film.type-film {
+        transition: opacity 400ms ease;
+    }
+    article.film.type-film.fade-out {
+        opacity: 0;
+    }
+    </style>
+    <script>
+    for (const button of document.querySelectorAll(".mn-filmmarks__ajax-button")) {
+        button.addEventListener("ajax:change", (ev) => {
+            if (ev.detail.action === "drop") {
+                const filmWrapper = ev.target.closest("article.film.type-film");
+                filmWrapper.classList.add("fade-out");
+                setTimeout(() => {
+                     filmWrapper.parentElement.removeChild(filmWrapper);
+                }, 400)
+            }
+        })
+    }
+    </script>
 </main>
 
 <?php get_template_part('template-parts/footer-menus-widgets'); ?>
