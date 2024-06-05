5618f90e74a1315abcec3c6922d694ade70647ef
Lucas García <lucas@codeccoop.org>
2024-06-05 11:23:56 +0200

    fix: mn_parse_duration and mn_filter_rich_text

--- includes/endpoints/catalog.php
+++ includes/endpoints/catalog.php
@@ -4,13 +4,21 @@ function mn_parse_duration($duration)
 {
     if (empty($duration)) return null;
         
-    preg_match('/(\d+) *min( (\d+) *s)?/', $duration, $matches);
-    if (empty($matches)) return -1;
+    preg_match('/(?:(\d+) *h[^\d]*)?(\d+) *m[^\d]*(?:(\d+) *s)?/', $duration, $matches);
+  if (empty($matches)) return -1;
         
-    $minutes = (int) $matches[1];
-    $seconds = (int) $matches[2];
+   $houres = (int) $matches[1];
+   $minutes = (int) $matches[2];
+   $seconds = (int) $matches[3];
 
-    return $minutes * 60 + $seconds;
+    return $houres * 3600 + $minutes * 60 + $seconds;
+}
+
+function mn_filter_rich_text($text)
+{
+    if (empty($text)) return $text;
+
+    return preg_replace('/(\n|\r|&nbsp;)+/', ' ', strip_tags($text));
 }
 
 function mn_get_catalog($data)
@@ -87,13 +95,13 @@ function mn_get_catalog($data)
             'title' => $film->post_title,
             'slug' => $film->post_name,
             'vimeo_id' => $custom_fields['vimeo_id'][0],
-            'excerpt' => $custom_fields['description'][0],
+            'excerpt' => mn_filter_rich_text($custom_fields['description'][0]),
             'featured_media' => isset($custom_fields['poster'][0]) ? wp_get_attachment_image_url($custom_fields['poster'][0]) : null,
             'featured_cover' => isset($custom_fields['cover'][0]) ? wp_get_attachment_image_url($custom_fields['cover'][0]) : null,
-            'sinopsis' => $custom_fields['long_description'][0],
+            'sinopsis' => mn_filter_rich_text($custom_fields['long_description'][0]),
             'language' => $custom_fields['language'][0],
             'subtitles' => $custom_fields['subtitles'][0],
-            'year' => $custom_fields['year'][0],
+            'year' => (int) $custom_fields['year'][0],
             'duration' => mn_parse_duration($custom_fields['duration'][0]),
             'age' => $custom_fields['age'][0],
             'geographic_area' => $zona_geografica[0]->name,
